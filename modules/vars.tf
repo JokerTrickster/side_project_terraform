517096a3bac3adb7a5bda6169b79ec497c9ee422
RyanBreathings <ryan@breathings.co.kr>
2023-11-19 00:27:56 +0900

    vpc init

---
+++ modules/vars.tf
@@ -0,0 +1,35 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "ECS_INSTANCE_TYPE" {
+  default = "t2.micro"
+}
+
+variable "ECS_AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-1924770e"
+  }
+}
+
+# Full List: http://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-0f5c19168a4bb46d3"
+  }
+}
+
+variable "INSTANCE_DEVICE_NAME" {
+  default = "/dev/xvdh"
+}
