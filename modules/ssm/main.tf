3cf097e00bb8a68f4ccfb8fb6e1059a3b7e05e21
JokerTrickster <pkjhj485@gmail.com>
2024-08-31 23:24:02 +0900

    {improvement} - ssm 통합 작업 진행 완료 \n #14

--- modules/ssm/main.tf
+++ modules/ssm/main.tf
@@ -1,27 +1,86 @@
+/*
+    ssm 네이밍 규칙
+  {환경}_{프로젝트명}_*
+*/
 
 
+// TODO 추후에 for문을 사용하여 프로젝트마다 생성해서 코드 간결화 필요
 
-resource "aws_ssm_parameter" "dev_mysql_user" {
-  name  = "dev_mysql_user"
+
+// common ssm
+
+resource "aws_ssm_parameter" "dev_common_mysql_port" {
+  name  = "dev_common_mysql_port"
   type  = "String"
-  value = var.mysql_user
+  value = var.dev_common_mysql_port
 }
 
-resource "aws_ssm_parameter" "dev_mysql_db" {
-  name  = "dev_mysql_db"
-  type  = "String"
-  value = var.mysql_db
-}
-
-resource "aws_ssm_parameter" "dev_mysql_port" {
-  name  = "dev_mysql_port"
-  type  = "String"
-  value = var.mysql_port
-}
-
-resource "aws_ssm_parameter" "dev_mysql_host"{
-  name = "dev_mysql_host"
+resource "aws_ssm_parameter" "dev_common_mysql_host"{
+  name = "dev_common_mysql_host"
   type = "String"
-  value = var.mysql_host
+  value = var.dev_common_mysql_host
 }
 
+
+resource "aws_ssm_parameter" "dev_common_redis_port" {
+  name  = "dev_common_redis_port"
+  type  = "String"
+  value = var.dev_common_redis_port
+}
+
+resource "aws_ssm_parameter" "dev_common_redis_host" {
+  name  = "dev_common_redis_host"
+  type  = "String"
+  value = var.dev_common_redis_host
+}
+
+
+// frog 프로젝트 ssm 
+resource "aws_ssm_parameter" "dev_frog_mysql_user" {
+  name  = "dev_frog_mysql_user"
+  type  = "String"
+  value = var.dev_frog_mysql_user
+}
+
+resource "aws_ssm_parameter" "dev_frog_mysql_db" {
+  name  = "dev_frog_mysql_db"
+  type  = "String"
+  value = var.dev_frog_mysql_db
+}
+
+resource "aws_ssm_parameter" "dev_frog_redis_user" {
+  name  = "dev_frog_redis_user"
+  type  = "String"
+  value = var.dev_frog_redis_user
+}
+
+resource "aws_ssm_parameter" "dev_redis_db" {
+  name  = "dev_frog_redis_db"
+  type  = "String"
+  value = var.dev_frog_redis_db
+}
+
+// food 프로젝트 ssm
+resource "aws_ssm_parameter" "dev_food_mysql_user" {
+  name  = "dev_food_mysql_user"
+  type  = "String"
+  value = var.dev_food_mysql_user
+}
+
+resource "aws_ssm_parameter" "dev_food_mysql_db" {
+  name  = "dev_food_mysql_db"
+  type  = "String"
+  value = var.dev_food_mysql_db
+}
+
+resource "aws_ssm_parameter" "dev_food_redis_user" {
+  name  = "dev_frog_redis_user"
+  type  = "String"
+  value = var.dev_food_redis_user
+}
+
+resource "aws_ssm_parameter" "dev_food_redis_db" {
+  name  = "dev_food_redis_db"
+  type  = "String"
+  value = var.dev_food_redis_db
+}
--- modules/ssm/variables.tf
+++ modules/ssm/variables.tf
@@ -1,19 +1,65 @@
-variable "mysql_user" {
+// common 변수
+variable "dev_common_mysql_port" {
+    description = "MySQL port"
+    default = "3306"
+}
+variable "dev_common_mysql_host" { 
+    description = "MySQL host"
+    default = "localhost"
+}
+
+variable "dev_common_redis_port" {
+    description = "Redis port"
+    default = "6379"
+}
+
+variable "dev_common_redis_host" {
+    description = "Redis host"
+    default = "localhost"
+}
+
+
+//frog 프로젝트 변수
+variable "dev_frog_mysql_user" {
     description = "MySQL user"
     default = "frog"
 }
 
-variable "mysql_db" {
+variable "dev_frog_mysql_db" {
     description = "MySQL database"
     default = "frog_dev"
 }
+variable "dev_frog_redis_user" {
+    description = "Redis user"
+    default = "frog"  
+}
 
-variable "mysql_port" {
-    description = "MySQL port"
-    default = "3306"
+variable "dev_frog_redis_db" {
+    description = "Redis database"
+    default = "1"
 }
-variable "mysql_host" { 
-    description = "MySQL host"
-    default = "localhost"
+
+
+//food 변수
+variable "dev_food_mysql_user" {
+    description = "MySQL user"
+    default = "food"
 }
-  
+
+variable "dev_food_mysql_db" {
+    description = "MySQL database"
+    default = "dev_food"
+}
+
+
+
+
+variable "dev_food_redis_user" {
+    description = "Redis user"
+    default = "food"  
+}
+
+variable "dev_food_redis_db" {
+    description = "Redis database"
+    default = "0"
+}
