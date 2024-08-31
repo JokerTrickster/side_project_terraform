34265c14c8c8c328121d417622d3de4f0bf7f449
JokerTrickster <pkjhj485@gmail.com>
2024-09-01 00:09:35 +0900

    {improvement} - ecr 통합 작업 완료 \n #14

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,10 +1,24 @@
-resource "aws_ecr_repository" "ecr_repository" {
-  name                 = "${var.environment}_${var.cluster}"
+/*
+    네이밍 규칙
+    {환경}_{프로젝트명}_*
+*/
+
+// frog 프로젝트 
+resource "aws_ecr_repository" "dev_frog_ecr_repository" {
+  name                 = "dev_frog_repository"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  } 
+}
+
+// food 프로젝트
+resource "aws_ecr_repository" "dev_food_ecr_repository" {
+  name                 = "dev_food_repository"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
   }
-
-  
 }
