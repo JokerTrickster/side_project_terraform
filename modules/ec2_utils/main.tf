443dc1a3861a0228a54eeb843c032bd7f4dbd88f
JokerTrickster <pkjhj485@gmail.com>
2024-09-01 00:16:06 +0900

    {improvement} - ec2 utils 통합 작업 완료 \n #14

--- modules/ec2_utils/main.tf
+++ modules/ec2_utils/main.tf
@@ -1,10 +1,11 @@
 module "ssm" {
   source      = "../ssm"
-  mysql_host = aws_eip.utils_eip[0].public_ip
+  dev_common_mysql_host = aws_eip.dev_common_utils_eip[0].public_ip
+  dev_common_redis_host = aws_eip.dev_common_utils_eip[0].public_ip
 }
 
-resource "aws_security_group" "utils_sg" {
-  name        = "utils_security_group"
+resource "aws_security_group" "dev_common_utils_sg" {
+  name        = "dev_common_utils_security_group"
   description = "Allow SSH access from all IPs"
 
   vpc_id = var.vpc_id
@@ -28,6 +29,15 @@ resource "aws_security_group" "utils_sg" {
     protocol    = "tcp"
     cidr_blocks = [var.vpc_cidr]
   }
+  // redis port
+   ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+
 
   egress {
     from_port   = 0
@@ -37,28 +47,28 @@ resource "aws_security_group" "utils_sg" {
   }
 
   tags = {
-    Name = "utils server Security Group"
+    Name = "dev utils server Security Group"
   }
 }
 
 resource "aws_network_interface" "network_interface" {
   subnet_id   = var.subnet_ids[1]
-  security_groups = [aws_security_group.utils_sg.id]
+  security_groups = [aws_security_group.dev_common_utils_sg.id]
   
   tags = {
-    Name = "primary_network_interface"
+    Name = "dev primary_network_interface"
   }
 }
 
-resource "aws_eip" "utils_eip"{
+resource "aws_eip" "dev_common_utils_eip"{
   domain = "vpc"
   count = 1
-  instance = aws_instance.utils_instance.id
+  instance = aws_instance.dev_common_utils_instance.id
     tags = {
-        Name = "utils-eip"
+        Name = "dev_common_utils_eip"
     }
 }
-resource "aws_instance" "utils_instance" {
+resource "aws_instance" "dev_common_utils_instance" {
   ami = "ami-0e4b1df799f55b8bb" //arm64
   instance_market_options {
     market_type = "spot"
@@ -77,7 +87,7 @@ resource "aws_instance" "utils_instance" {
   user_data = file("${path.module}/templates/user_data.sh")
 
   tags = {
-    Name = "utils-spot"
+    Name = "dev_common_utils_spot"
   }
 }
 
